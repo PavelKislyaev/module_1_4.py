d742ca7299d2773beb1e03900e22eb7f71a072d2
PavelKislyaev <pavel.kislyayev@mail.ru>
2024-08-21 17:34:03 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,13 @@
+my_string=input("Количество символов: ")
+len=input("Количество символов: ")
+print(my_string ,"символа" )
+print('my_string'.upper())
+print('my_string'.lower())
+print('my _ string'.replace(' ' ,''))
+print('my _ string'.replace(' ' ,'#'))
+print('my_string'[1])
+print('my_string'[8])
+
+
+
+
